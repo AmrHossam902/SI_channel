91383cfb142bcde244e375b921793cd198f75d86
AntounWagdy <Antounwagdy@gmail.com>
2016-04-25 22:04:17 +0200

    user info list adaper

---
+++ app/src/main/java/cairo_university/si_channel/My_info_adapter.java
@@ -0,0 +1,21 @@
+rial_7;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+
+public class My_info_adapter extends CursorAdapter {
+    public My_info_adapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.my_info_item_view, viewGroup, false);
+
+    }
