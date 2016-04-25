b907522cd9611e9bd83d5392a8163f20ee60fbc2
malakfbebawy <malakfahim144@gmail.com>
2016-04-25 22:24:46 +0200

    2nd update
    insertion and deletion for posts and comments are added

--- app/src/main/java/cairo_university/si_channel/Query_Manager.java
+++ app/src/main/java/cairo_university/si_channel/Query_Manager.java
@@ -52,12 +52,51 @@ public class Query_Manager {
 
     }
 
-
     public long InsertPost(String content, long Time, String user) {
         Sql="insert into Disc_Post(Content,Date_time,Maker_id) values('"+content+"','"+Time+"','"+user+"')";
         return DBM.ExecuteInsert(Sql);
     }
 
+    public Cursor selectDiscByID(int id) {
+        Sql="select Content as _id,Date_time,Name from Disc_Post,Stud where Disc_Post.ID='"+id+"'and Maker_id=Stud.ID";
+        return DBM.ExecuteSelect(Sql);
+    }
 
+    public Cursor selectCommentsByDiscID(int id) {
+        Sql="Select Content as _id,Name from Comment,Stud where Post_id=\"1\" and Maker_id=Stud.ID";
+        return DBM.ExecuteSelect(Sql);
+
+    }
+
+    public long insertComment(String x,int id) {
+        Sql="insert into Comment(Content,Post_id,Maker_id) values('"+x+"',"+id+","+MainActivity.getUser()+")";
+        return DBM.ExecuteInsert(Sql);
+    }
+
+    public Cursor Select_info_of_id(String user) {
+        Sql="Select Content as _id ,Date_time,Grade from Info_Post,INS_ADM where INS_ADM.ID=Info_Post.Inst_ID and INS_ADM.ID='"+user+"'";
+        return DBM.ExecuteSelect(Sql);
+    }
+
+    public Long Add_info(String s,int Grade) {
+        Sql="insert into Info_Post(Content,Date_time,Inst_ID,Grade) Values ('"+s+"','"+System.currentTimeMillis()+"','"+MainActivity.getUser()+"','"+Grade+"');";
+        return DBM.ExecuteInsert(Sql);
+
+    }
+
+    public long Delete_Instructor(String id) {
+
+        return DBM.ExecuteDelete("INS_ADM", "ID = '" + id + "'");
+    }
+
+    public long Delete_Student(String s) {
+        return DBM.ExecuteDelete("Stud", "ID = '" + s + "'");
+    }
+
+    public long insertStudent(String id, String s, String s1, int selectedItem) {
+        Sql="insert into Stud values('"+id+"','"+s+"','"+s1+"','"+selectedItem+"')";
+        return DBM.ExecuteInsert(Sql);
+
+    }
 	
 }
