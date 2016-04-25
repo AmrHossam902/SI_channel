6e29875beed088118af6d9ea1a0ead5d816daa65
malakfbebawy <malakfahim144@gmail.com>
2016-04-25 21:43:20 +0200

    2nd update

--- app/src/main/java/cairo_university/si_channel/MainActivity.java
+++ app/src/main/java/cairo_university/si_channel/MainActivity.java
@@ -1,13 +1,33 @@
-package cairo_university.si_channel;
+package com.example.antou.trial_7;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText[]e;
+    Query_Manager QM;
+    static String User;
+    static int Type; // 0: for students 1: Inst and admins
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User="";
+        e=new EditText[2];
+        e[0]=(EditText)findViewById(R.id.username_entry);
+        e[1]=(EditText)findViewById(R.id.password_entry);
+        QM=Query_Manager.Create_manager(this.getApplicationContext());
+        QM.Backup();
+
     }
+    
 }
