23f3eda6797014acf915f7fcce9c0c54b66a47cf
AntounWagdy <Antounwagdy@gmail.com>
2016-04-26 00:24:22 +0200

    version 2

--- app/src/main/java/cairo_university/si_channel/Home.java
+++ app/src/main/java/cairo_university/si_channel/Home.java
@@ -11,15 +11,37 @@ import android.view.MenuItem;
 
 public class Home extends AppCompatActivity {
 
+    TabLayout TL;
+    ViewPager VP;
    static Context X;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        X=getApplicationContext();
+        X = getApplicationContext();
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_home);
+        if (MainActivity.getType() == 0) {
+            TL = (TabLayout) findViewById(R.id.tabLayout);
+            VP = (ViewPager) findViewById(R.id.viewPager);
+            ViewPagerAdapter VPA = new ViewPagerAdapter(getSupportFragmentManager());
+            VPA.add_fragements(new Info_Frags(), "Information");
+            VPA.add_fragements(new Survey_frag(), "Survey");
+            VPA.add_fragements(new Disc_frag(), "Discussion");
+            VP.setAdapter(VPA);
+            TL.setupWithViewPager(VP);
+        }
+        else
+        {
+            TL = (TabLayout) findViewById(R.id.tabLayout);
+            VP = (ViewPager) findViewById(R.id.viewPager);
+            ViewPagerAdapter VPA = new ViewPagerAdapter(getSupportFragmentManager());
+            VPA.add_fragements(new Admin_info() , "My Information");
+            VPA.add_fragements(new Survey_frag(), "Survey");
+            VPA.add_fragements(new Add_Delete_user(), "Control Users");
+            VP.setAdapter(VPA);
+            TL.setupWithViewPager(VP);
+        }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
