f3fc1b88772a6793dc98c5c32aa644468e21288d
malakfbebawy <malakfahim144@gmail.com>
2016-05-23 20:07:07 +0200

    Add files via upload

---
+++ app/src/main/java/cairo_university/si_channel/Student_Survey_frag.java
@@ -0,0 +1,33 @@
+package cairo_university.si_channel2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Student_Survey_frag extends Fragment {
+
+
+    public Student_Survey_frag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+     //   View v=inflater.inflate(R.layout.fragment_student__survey_frag_item, container, false);
+
+
+        return null;
+    }
+
+
+}
