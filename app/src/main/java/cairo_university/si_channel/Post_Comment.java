0ad3508bc1a7b643f58d674183be70be820e1ca1
malakfbebawy <malakfahim144@gmail.com>
2016-05-23 20:04:34 +0200

    Add files via upload

--- app/src/main/java/cairo_university/si_channel/Post_Comment.java
+++ app/src/main/java/cairo_university/si_channel/Post_Comment.java
@@ -1,9 +1,9 @@
-package com.example.antou.trial_7;
+package cairo_university.si_channel2;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,13 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class Post_Comment extends AppCompatActivity {
 
     Query_Manager QM;
-    Cursor C;
-    Cursor C2;
+    JSONArray JA;
+    JSONArray comments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +27,11 @@ public class Post_Comment extends AppCompatActivity {
         setContentView(R.layout.activity_post__comment);
         Intent I=this.getIntent();
         final int id =I.getIntExtra("POST_ID",0);
-        QM=Query_Manager.Create_manager(getApplicationContext());
-        C=QM.selectDiscByID(id);
-        C2=QM.selectCommentsByDiscID(id);
+        QM=Query_Manager.Create_QM();
+
+        JA = QM.select_disc_by_ID2(id);
+        comments = QM.select_comments(id);
+        System.out.println("OM EL ID:  "+ id);
         initializepost();
         initializeComments();
 
@@ -39,8 +43,8 @@ public class Post_Comment extends AppCompatActivity {
                 if (ET.getText().toString().isEmpty())
                     return;
 
-                QM.insertComment(ET.getText().toString(),id);
-                C2=QM.selectCommentsByDiscID(id);
+                QM.Insert_comment(Integer.parseInt(MainActivity.getUser()),id,ET.getText().toString());
+                comments = QM.select_comments(id);
                 initializeComments();
                 ET.setText("");
             }
@@ -49,48 +53,52 @@ public class Post_Comment extends AppCompatActivity {
 
     private void initializeComments() {
         ListView LV=(ListView)findViewById(R.id.listView3);
-        Comment_Adapter I=new Comment_Adapter(getApplicationContext(),C2,0);
+        Comment_Adapter I=new Comment_Adapter(comments, Home.X);
         LV.setAdapter(I);
     }
 
     private void initializepost() {
 
-        C.moveToFirst();
         TextView TV[];
         TV=new TextView[3];
 
         TV[0]=(TextView)findViewById(R.id.textView);
         TV[1]=(TextView)findViewById(R.id.textView2);
         TV[2]=(TextView)findViewById(R.id.textView3);
-        TV[0].setText(C.getString(C.getColumnIndex("Name")));
-        TV[1].setText(C.getString(C.getColumnIndex("_id")));
-
-        double d=C.getDouble(C.getColumnIndexOrThrow("Date_time"));
-        double x=System.currentTimeMillis();
-        int Creation= (int) ((x-d)/60000);
-        TV[2].setText(Creation+" minutes ago.");
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_post__comment, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        try {
+            TV[0].setText(JA.getJSONObject(0).getString("Name"));
+            TV[1].setText(JA.getJSONObject(0).getString("Content"));
+            TV[2].setText(JA.getJSONObject(0).getString("Creation_date"));
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+//
+//        double d=C.getDouble(C.getColumnIndexOrThrow("Date_time"));
+//        double x=System.currentTimeMillis();
+//        int Creation= (int) ((x-d)/60000);
+//        TV[2].setText(Creation+" minutes ago.");
 
-        return super.onOptionsItemSelected(item);
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_post__comment, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
