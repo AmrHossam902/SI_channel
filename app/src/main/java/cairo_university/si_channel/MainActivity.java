c23b23dd6fb0c662861905cfa2934e74b4e7c978
malakfbebawy <malakfahim144@gmail.com>
2016-05-23 19:50:08 +0200

    Add files via upload

--- app/src/main/java/cairo_university/si_channel/MainActivity.java
+++ app/src/main/java/cairo_university/si_channel/MainActivity.java
@@ -1,14 +1,17 @@
-package com.example.antou.trial_7;
+package cairo_university.si_channel2;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     Query_Manager QM;
     static String User;
     static int Type; // 0: for students 1: Inst and admins
+    static String IA_type;
+    static int ID;
+    static int G_year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,62 +31,76 @@ public class MainActivity extends AppCompatActivity {
         e=new EditText[2];
         e[0]=(EditText)findViewById(R.id.username_entry);
         e[1]=(EditText)findViewById(R.id.password_entry);
-        QM=Query_Manager.Create_manager(this.getApplicationContext());
-        QM.Backup();
+        QM=Query_Manager.Create_QM();
+        //QM.Backup();
 
     }
-    
-    
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//      //  getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+////        if (id == R.id.action_settings) {
+////            return true;
+////        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
-	
-    
-	
     public void Sign_in(View view)
     {
         String USER=e[0].getText().toString();
         String Pass=e[1].getText().toString();
 
-        Cursor C=QM.isUser(USER, Pass);
+        if(USER.equals("") || Pass.equals(""))
+        {
+            Toast.makeText(this,"fields should be filled",Toast.LENGTH_LONG).show();
+            return;
+        }
 
+        JSONArray JA = QM.select_stud(Integer.parseInt(USER), Pass);
 
-        if (C.moveToFirst())
+        if (JA.length()!=0)
         //change last time log in
         {
-            Toast.makeText(this.getApplicationContext(), "Successfully signed in :D", Toast.LENGTH_LONG).show();
+            Toast.makeText(this.getApplicationContext(), "student Successfully signed in :D", Toast.LENGTH_LONG).show();
             User=USER;
             Type=0;
+            IA_type = "";
+            try {
+                G_year = Integer.parseInt(JA.getJSONObject(0).getString("Grade_year"));
+            } catch (JSONException e1) {
+                e1.printStackTrace();
+            }
+            //    Toast.makeText(this, "you are a student ",Toast.LENGTH_LONG);
             Intent intent =new Intent(this.getApplicationContext(),Home.class);
             startActivity(intent);
         }
         else
         {
-            Cursor c2 = QM.isInst(USER,Pass);
-            if (c2.moveToFirst())
+            JA = QM.select_IA(Integer.parseInt(USER), Pass);
+            if (JA.length()!=0)
             {
                 User=USER;
+                try {
+                    IA_type = JA.getJSONObject(0).getString("Type");
+                    ID = JA.getJSONObject(0).getInt("ID");
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
+                }
                 Type=1;
-                Toast.makeText(this.getApplicationContext(), "Successfully signed in :D", Toast.LENGTH_LONG).show();
+                Toast.makeText(this.getApplicationContext(), "ins Successfully signed in :D", Toast.LENGTH_LONG).show();
                 Intent intent =new Intent(this.getApplicationContext(),Home.class);
                 startActivity(intent);
             }
@@ -88,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this.getApplicationContext(), "User Name or password is wrong", Toast.LENGTH_LONG).show();
         }
 
-		        C.close();
+       // C.close();
     }
 
     public static int getType() {return Type;}
@@ -96,5 +116,4 @@ public class MainActivity extends AppCompatActivity {
     {
         return User;
     }
-
 }
