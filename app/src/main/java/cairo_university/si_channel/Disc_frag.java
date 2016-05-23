9eebae2ca6eb0b0920e9efb096cff858e97dc2da
AmrHossam <amrhossam902@gmail.com>
2016-05-23 14:20:16 +0200

    Add files via upload

--- app/src/main/java/cairo_university/si_channel/Disc_frag.java
+++ app/src/main/java/cairo_university/si_channel/Disc_frag.java
@@ -1,4 +1,4 @@
-package com.example.antou.trial_7;
+package cairo_university.si_channel2;
 
 
 import android.content.Intent;
@@ -9,10 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 
 /**
@@ -37,53 +41,67 @@ public class Disc_frag extends Fragment {
         // Inflate the layout for this fragment
         // Inflate the layout for this fragment
 
-        QM=Query_Manager.Create_manager(Home.X);
-        C=QM.Select_Disc();
+        QM=Query_Manager.Create_QM();
+        final JSONArray JA =QM.select_disc();
         View view = inflater.inflate(R.layout.fragment_disc_frag, container, false);
         LV=(ListView)view.findViewById(R.id.listView2);
         ET=(EditText)view.findViewById(R.id.editText);
         B=(Button)view.findViewById(R.id.button);
-        //Create the adapter
-        C.moveToFirst();
-        Information_adapter I=new Information_adapter(Home.X,C,0);
+//        //Create the adapter
+//        C.moveToFirst();
+        stud_info_adapter I=new stud_info_adapter(JA,Home.X);
         LV.setAdapter(I);
-        LV.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+
+        LV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                C.moveToPosition(i);
-                int id =C.getInt(C.getColumnIndex("ID"));
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Home.X,"AAAAAA"+ position,Toast.LENGTH_LONG).show();
                 Intent I=new Intent(Home.X,Post_Comment.class);
-                I.putExtra("POST_ID",id);
+                try {
+                    I.putExtra("POST_ID",JA.getJSONObject(position).getInt("ID"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 startActivity(I);
             }
         });
-        B.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                post(view);
-                return;
-            }
-        });
 
+//
 
+//        LV.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                C.moveToPosition(i);
+//                int id =C.getInt(C.getColumnIndex("ID"));
+//                Intent I=new Intent(Home.X,Post_Comment.class);
+//                I.putExtra("POST_ID",id);
+//                startActivity(I);
+//            }
+//        });
+//        B.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                post(view);
+//                return;
+//            }
+//        });
         return view;
     }
-	
-	
-	public void post(View view)
-    {
-        String x=ET.getText().toString();
-        if (x.isEmpty())
-            return;
-        QM.InsertPost(x,System.currentTimeMillis(),MainActivity.getUser());
-        C=QM.Select_Disc();
-        C.moveToFirst();
-        Information_adapter I=new Information_adapter(Home.X,C,0);
-        LV.setAdapter(I);
-        ET.setText("");
-        return;
 
+    public void post(View view)
+    {
+//        String x=ET.getText().toString();
+//        if (x.isEmpty())
+//            return;
+//        QM.InsertPost(x,System.currentTimeMillis(),MainActivity.getUser());
+//        C=QM.Select_Disc();
+//        C.moveToFirst();
+//        Information_adapter I=new Information_adapter(Home.X,C,0);
+//        LV.setAdapter(I);
+//        ET.setText("");
+//        return;
+//
 
     }
 }
