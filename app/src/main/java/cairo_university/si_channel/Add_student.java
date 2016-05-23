6bebc8bb2bfa290b9f0ecf7eca96f687f6a729bc
malakfbebawy <malakfahim144@gmail.com>
2016-05-23 13:34:12 +0200

    Add files via upload

--- app/src/main/java/cairo_university/si_channel/Add_student.java
+++ app/src/main/java/cairo_university/si_channel/Add_student.java
@@ -1,9 +1,9 @@
-package com.example.antou.trial_7;
+package cairo_university.si_channel2;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,7 +20,6 @@ public class Add_student extends AppCompatActivity {
     Button B;
     Spinner Sp;
     Context C;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,49 +32,52 @@ public class Add_student extends AppCompatActivity {
         Sp=(Spinner)findViewById(R.id.spinner3);
 
         C=this.getApplicationContext();
-        String[] X=new String[4];
-        for (int i=1;i<5;i++)
+        String[] X=new String[5];
+        for (int i=1;i<6;i++)
             X[i-1]=""+i;
 
         Sp.setSelection(0);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, X);
         Sp.setAdapter(adapter);
         B=(Button)findViewById(R.id.button6);
-        QM=Query_Manager.Create_manager(this.getApplicationContext());
-        
+        QM=Query_Manager.Create_QM();
         B.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 long y = QM.insertStudent(id, ET[0].getText().toString(), ET[1].getText().toString(),  Sp.getSelectedItemPosition()+1);
+                if(ET[0].getText().toString().isEmpty() || ET[1].getText().toString().isEmpty())
+                {
+                    Toast.makeText(C,"fields shouldn't be left empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                 int y = QM.Insert_student(Integer.parseInt(id), ET[1].getText().toString(), Integer.parseInt(Sp.getSelectedItem().toString()) ,ET[0].getText().toString());
 
-                 if (y == -1)
+                 if (y == 0)
                      Toast.makeText(C, "This id is already exist", Toast.LENGTH_SHORT).show();
                 else Toast.makeText(C, "Done Adding!", Toast.LENGTH_SHORT).show();
-                Intent I=new Intent(C,Home.class);
-                startActivity(I);
-            }
+                    Add_student.super.onBackPressed();
+                  }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_student, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_add_student, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
